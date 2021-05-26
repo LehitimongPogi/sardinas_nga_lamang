06ecf9c8743964b3952b2a4d9e8dc5dffe5b55e6
Mike Wazowski <johnglenfalceso@protonmail.com>
2021-05-26 11:11:25 +0800

    added files on iisda branch

---
+++ cfile3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main()
+{
+    int x, y;
+    x = 5, y = 8; 
+    printf("%d + %d = %d", x, y, x+y);
+    printf("Hello world!");
+    printf("Hello world!");
+    printf("Hello world!");
+    printf("Hello world!");
+}
