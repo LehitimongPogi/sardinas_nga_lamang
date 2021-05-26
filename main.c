09d7e1aad22f21b65537e36536db4a3671a8cfed
Mike Wazowski <johnglenfalceso@protonmail.com>
2021-05-26 11:53:14 +0800

    deleted cfile3.c, modified main.c and readme.md

--- main.c
+++ main.c
@@ -10,16 +10,3 @@ void main()
     printf("Hello world!");
     printf("Hello world!");
 }
-
-#include <stdio.h>
-
-void main()
-{
-    int x, y;
-    x = 5, y = 8; 
-    printf("%d + %d = %d", x, y, x+y);
-    printf("Hello world!");
-    printf("Hello world!");
-    printf("Hello world!");
-    printf("Hello world!");
-}
